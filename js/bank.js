6e9481081eef78047e84b4aee8998d6299bd15ef
IsmielAbir <ismielabir286@gmail.com>
2022-02-12 20:34:32 +0600

    first commit

---
+++ js/bank.js
@@ -0,0 +1,20 @@
+document.getElementById('submit').addEventListener('click', 
+function(){
+
+    //user-email
+
+    const emailfield = document.getElementById('user-email');
+    const email = emailfield.value;
+
+    //pass
+
+    const password = document.getElementById('user-password');
+    const pass = password.value;
+
+    if(email == 'abc@gmail.com'  && pass == '123abc'){
+        console.log("valid");
+        window.location.href = 'banking.html';
+    }
+
+
+})
