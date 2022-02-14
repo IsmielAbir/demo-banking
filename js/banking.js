fb498cdb0dd7d94f97f4d0ba79a7b777c6154f5a
IsmielAbir <ismielabir286@gmail.com>
2022-02-14 21:11:54 +0600

    added

--- js/banking.js
+++ js/banking.js
@@ -4,7 +4,7 @@ document.getElementById('deposite').addEventListener('click', function(){
     const deposite = document.getElementById('deposite-amount')
     const amount = deposite.value;
 
-
+// update deposite total
     const depositeTotal = document.getElementById('deposite-total');
 
     const currentAmount = depositeTotal.innerText;
@@ -12,6 +12,14 @@ document.getElementById('deposite').addEventListener('click', function(){
 
 
     depositeTotal.innerText = newdeposite;
+
+    //update account balance 
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceText = balanceTotal.innerText;
+    const previousBalance = parseFloat(balanceText);
+    const newBalance = previousBalance + newdeposite;
+    balanceTotal.innerText = newBalance;
+
 
     //clear the deposite input 
     deposite.value = '';
